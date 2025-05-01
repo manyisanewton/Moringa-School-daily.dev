54cc0b7e4cac041cee70e372dc21af84b989c2e5
shawukie4them <shawukiekawuki@gmail.com>
2025-05-01 12:21:34 +0300

    Configured SQLAlchemy and Flask-Migrate in app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import DevConfig
-
+db = SQLAlchemy()
+migrate = Migrate()
 def create_app(config_class=DevConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    db.init_app(app)
+    migrate.init_app(app, db)
     return app
