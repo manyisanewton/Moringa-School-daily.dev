2e79d7d07119a81ac3bdf0efe2406c9d7438a6ed
Newton Manyisa <manyisanewton26@gmail.com>
2025-05-02 06:30:21 +0300

    cleaned up all the files for new backend stucture to avoid conflicts

--- backend/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.auth_routes import auth_bp
-
-app = create_app()
-app.register_blueprint(auth_bp, url_prefix='/auth')
-
-if __name__ == "__main__":
-    app.run(debug=True)
